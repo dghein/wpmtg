7f6ac7e24e5ffe793df3406da38417cf148eec0e
Dustin Hein <dhein@resulta.com>
2020-08-30 12:57:22 -0300

    less confusing labels for card set taxonomy

--- wpmtg.php
+++ wpmtg.php
@@ -293,10 +293,22 @@ function wpmtg_register_card_post_type()
         'wpmtg_card_setname',
         'wpmtg_magiccard',
         array(
+            'labels' => array(
+                'name' => 'Card Sets',
+                'singular_name' => 'Card Set',
+                'search_items' => 'Search Card Sets',
+                'popular_items' => 'Popular Card Sets',
+                'all_items' => 'All Card Sets',
+                'edit_item' => 'Edit Card Set',
+                'view_item' => 'View Card Set',
+                'update_item' => 'Update Card Set',
+                'add_new_item' => 'Add New Card Set'
+            ),
             'rewrite' => array(
                 'slug' => 'set',
                 'with_front' => false
             ),
+            'query_var' => 'wpmtg_card_setname',
             'show_in_nav_menus' => true
         )
     );
