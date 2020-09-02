1e224d6085c209b650db19ca24382351417bea62
Dustin Hein <dhein@resulta.com>
2020-09-02 18:51:30 -0300

    version

--- wpmtg.php
+++ wpmtg.php
@@ -5,7 +5,7 @@ Plugin Name:  wpmtg
 Description:  A Magic: The Gathering plugin
 Plugin URI:   https://dustinsmodern.life
 Author:       Dustin Hein
-Version:      0.1.14
+Version:      0.1.17
 License:      GPL v2 or later
 License URI:  https://www.gnu.org/licenses/gpl-2.0.txt
 */
