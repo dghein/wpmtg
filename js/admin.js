224cd7de1a249e9d65c9264e17d45b07cd6086e1
Dustin Hein <dhein@resulta.com>
2024-07-27 16:31:09 -0300

    put import form event listener in conditional to avoid console error

--- js/admin.js
+++ js/admin.js
@@ -1,22 +1,25 @@
 document.addEventListener("DOMContentLoaded", (e) => {
   const wpmtgImportSetForm = document.querySelector('#frmImport');
-  wpmtgImportSetForm.addEventListener("submit", (e) => {
-    e.preventDefault();
 
-    fetch('/wp/wp-admin/admin-ajax.php', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: new URLSearchParams(new FormData(wpmtgImportSetForm))
-    }).then(response => {
-      return response;
-    }).then(jsonResponse => {
-      console.log(jsonResponse);
-      const form = document.querySelector('#frmImport');
-      const newDiv = document.createElement('div');
-      newDiv.innerHTML = 'You imported some cards with an ajax request!';
-      form.append(newDiv);
-    })
-  });
+  if (wpmtgImportSetForm) {
+    wpmtgImportSetForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+  
+      fetch('/wp/wp-admin/admin-ajax.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams(new FormData(wpmtgImportSetForm))
+      }).then(response => {
+        return response;
+      }).then(jsonResponse => {
+        console.log(jsonResponse);
+        const form = document.querySelector('#frmImport');
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML = 'You imported some cards with an ajax request!';
+        form.append(newDiv);
+      })
+    });
+  }
 });
